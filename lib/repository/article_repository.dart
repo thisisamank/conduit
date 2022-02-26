91e6459a4ce49340d042e97ce6105381b0f30a43
Aman Kumar <thisisamank@gmail.com>
2022-02-27 03:16:15 +0530

    feat: articles apis integrated

--- lib/repository/article_repository.dart
+++ lib/repository/article_repository.dart
@@ -16,6 +16,10 @@ abstract class BaseArticleRepository {
     String? favorited,
     int limit = ApiEndpoints.paginationLimit,
   });
+  Future<Either<TotalArticles, Failure>> fetchFeedArticles({
+    int limit = ApiEndpoints.paginationLimit,
+  });
+  Future<Either<Article, Failure>> getArticleFromSlug(String slug);
 }
 
 class ArticleRepository extends BaseArticleRepository {
@@ -46,16 +50,56 @@ class ArticleRepository extends BaseArticleRepository {
     int limit = ApiEndpoints.paginationLimit,
   }) async {
     try {
-      final response =
-          await _dio.get(ApiEndpoints.articlesUrl, queryParameters: {
-        'limit': limit,
-        if (tag != null) 'tag': tag,
-        if (author != null) 'author': author,
-        if (favorited != null) 'favorited': favorited,
-      });
+      final response = await _dio.get(
+        ApiEndpoints.articlesUrl,
+        queryParameters: {
+          'limit': limit,
+          if (tag != null) 'tag': tag,
+          if (author != null) 'author': author,
+          if (favorited != null) 'favorited': favorited,
+        },
+      );
       if (isSuccessfulResponse(response.statusCode!)) {
         final articleJson = response.data as Map<String, dynamic>;
         final article = TotalArticles.fromJson(articleJson);
+        return left(article);
+      }
+      return right(Failure());
+    } on DioError catch (e) {
+      String message = DioErrorUtil.handleError(e);
+      return right(Failure(message));
+    }
+  }
+
+  @override
+  Future<Either<TotalArticles, Failure>> fetchFeedArticles({
+    int limit = ApiEndpoints.paginationLimit,
+  }) async {
+    try {
+      final response = await _dio.get(
+        ApiEndpoints.feedArticlesUrl,
+        queryParameters: {'limit': limit},
+      );
+      if (isSuccessfulResponse(response.statusCode!)) {
+        final articlesJson = response.data as Map<String, dynamic>;
+        final article = TotalArticles.fromJson(articlesJson);
+        return left(article);
+      }
+      return right(Failure());
+    } on DioError catch (e) {
+      String message = DioErrorUtil.handleError(e);
+      return right(Failure(message));
+    }
+  }
+
+  @override
+  Future<Either<Article, Failure>> getArticleFromSlug(String slug) async {
+    try {
+      final response =
+          await _dio.get(ApiEndpoints.articleFromSlugUrl(slug: slug));
+      if (isSuccessfulResponse(response.statusCode!)) {
+        final articleJson = response.data as Map<String, dynamic>;
+        final article = Article.fromJson(articleJson);
         return left(article);
       }
       return right(Failure());
